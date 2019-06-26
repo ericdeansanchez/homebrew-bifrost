f527a3aba6088638d1dd010e725977ca511edea6
ericdeansanchez <ericdeansanchez@berkeley.edu>
2019-06-26 16:29:41 -0700

    corrects version number

--- bifrost.rb
+++ bifrost.rb
@@ -1,5 +1,5 @@
 class Bifrost < Formula
-    version '0.1.1'
+    version '0.1.0'
     desc "virtualization via containers"
     homepage "https://github.com/ericdeansanchez/bifrost"
     url "https://github.com/ericdeansanchez/bifrost/releases/download/v0.1.0-beta/bifrost-#{version}-x86_64-apple-darwin18.6.0.tar.gz"
