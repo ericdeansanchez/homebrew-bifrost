13153a4315da87b70148fc364472355e5de9ced3
ericdeansanchez <ericdeansanchez@berkeley.edu>
2019-07-06 16:14:22 -0700

    updated release

--- bifrost.rb
+++ bifrost.rb
@@ -3,7 +3,7 @@ class Bifrost < Formula
     desc "virtualization via containers"
     homepage "https://github.com/ericdeansanchez/bifrost"
     url "https://github.com/ericdeansanchez/bifrost/releases/download/v0.1.0-beta/bifrost-#{version}-x86_64-apple-darwin18.6.0.tar.gz"
-    sha256 "f31ae690879fdc9e7c9550a29f861b81660288f652e58725fe173eccf35ad1d5"
+    sha256 "102d82ffcbf2a7ad9d8f8707e1347ede008a47e3d41a6e452200f8800b999587"
 
     def install
       bin.install "bifrost"
