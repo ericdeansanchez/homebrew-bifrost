65e1c05842adda31bc81a3310c45bd7a50ec14de
eric <ericdeansanchez@berkeley.edu>
2019-10-04 12:19:03 -0700

    Update replicate.rb

--- replicate.rb
+++ replicate.rb
@@ -1,8 +1,8 @@
 class Replicate < Formula
-  version '0.1.0'
+  version '0.1.1'
   desc 'replicate generic cli applications'
   homepage 'https://github.com/ericdeansanchez/replicate'
-  url "https://github.com/ericdeansanchez/replicate/releases/download/v0.1.0/replicate-#{version}-x86_64-apple-darwin18.6.0.tar.gz"
+  url "https://github.com/ericdeansanchez/replicate/releases/download/v0.1.1/replicate-#{version}-x86_64-apple-darwin18.6.0.tar.gz"
   sha256 '41226f453a3053123b48ddfe473cb11154793cb22fe7a6127693538baf86fdf0'
 
   def install
